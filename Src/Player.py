3a67b7cb7d0ac759500d5d4363ecd1aa24eeaafa
Nevens-fr <aurelientudo@hotmail.fr>
2021-11-19 22:56:43 +0100

    Changed and enhancement
    tile for textures
    animated player
    remove border from texture
    better game over
    better collisions handling
    remove transparancy on spacebar
    added new methods for Draw
    antialiasing on texts
    best score text place changed
    icon image changer

--- Src/Player.py
+++ Src/Player.py
@@ -9,6 +9,9 @@ class joueur(exampleClass.example):
     #constructeur
     def __init__(self, coordX, coordY):
         self.img = Draw.createImg("Assets/piaf.png")
+        self.h = 29
+        self.w = 41
+        self.rect = Draw.createRect(0, 0, self.w, self.h)
         self.temps = Draw.getTime()
         self.coords = (coordX, coordY)
         self.coordX = coordX
@@ -18,14 +21,18 @@ class joueur(exampleClass.example):
         self.yBase = coordY
         self.direction = 0
 
+    def getRect(self):
+        return self.rect
+
     #affichage du joueur
     def drawPlayer(self, screen):
-        Draw.drawBlit(screen, self)
+        Draw.drawBlitRect(screen, self)
 
     #Fait chuter le joueur au fur et à mesure
     def updatePlayerChute(self):
         self.coordY +=2
         self.coords = (self.coordX, self.coordY)
+        self.rect.x = 0
 
     #Fait monter le joueur si la touche espace est activée
     def updatePlayerMovement(self, screen):
@@ -33,6 +40,7 @@ class joueur(exampleClass.example):
             self.coordY -= self.addMove
             self.temps = Draw.getTime()
             self.coords = (self.coordX, self.coordY)
+            self.rect.x = self.w
             self.drawPlayer(screen)
         elif self.temps + self.addTemps <= Draw.getTime():
             self.temps = Draw.getTime()
@@ -44,10 +52,12 @@ class joueur(exampleClass.example):
         if self.temps + self.addTemps <= Draw.getTime():
             if self.direction == 0 and self.coordY >= self.yBase - 20:
                 self.coordY -= 2
+                self.rect.x = self.w
             elif self.direction == 0 and self.coordY < self.yBase - 20:
                 self.direction = 1
             elif self.direction == 1 and self.coordY <= self.yBase + 20:
                 self.coordY += 2
+                self.rect.x = 0
             elif self.direction == 1 and self.coordY > self.yBase + 20:
                 self.direction = 0
 
@@ -66,16 +76,17 @@ class joueur(exampleClass.example):
         i1 = int((self.coordX + size) /size)#collission haut droite
         j1 = int(self.coordY / size)
 
-        i2 = int(self.coordX / size)    #collision bas gauche
+        i2 = int(self.coordX / self.h)    #collision bas gauche
         j2 = int((self.coordY + size) /size)
 
-        i2 = int((self.coordX + size) / size)    #collision bas droite
-        j2 = int((self.coordY + size) /size)
+        i2 = int((self.coordX + self.h) / size)    #collision bas droite
+        j2 = int((self.coordY + self.h  - 2) /size)
 
         try:
             if tab[j][i] == "0" and tab[j1][i1] == "0" and tab[j2][i2] == "0":
                 return False
             else:
+                Draw.attente(500)
                 return True
         except IndexError:
             return 0
